69b3fc769ab35197e21cfa0b60aabceec3474ffb
smaii1 <161672779+smaii1@users.noreply.github.com>
2026-08-17 02:01:34 +1000

    Added background, hole, mole sprite variations, fixed texture filters, and updated animations accordingly

--- src/game2.js
+++ src/game2.js
@@ -20,15 +20,18 @@ const gameState = {
     gameOver: false
 };
 
-const Mole_Active_Y = -100;
-
 function preload () {
 	//images (vfx, sprites, etc)
+  this.load.image('background', 'assets/background.png');
+  this.load.image('hole', 'assets/hole.png');
   this.load.image('hammer', 'assets/hammer1.png');
   this.load.image('mole', 'assets/mole.png'); // normal mole image (just popped up)
+  this.load.image('whacked-mole', 'assets/whacked-mole.png'); // whacked angry mole
+  this.load.image('missed-mole', 'assets/missed-mole.png'); // missed mole with stuck out tongue
 }
 
 function create () {
+  setBackground(this);
   this.input.setDefaultCursor('none'); //hide browser cursor on game screen
   this.input.on('pointerdown', () => {
     if (gameState.canWhack) {
@@ -51,8 +54,9 @@ function create () {
   ];
 
   holePositions.forEach(pos => {
-    const hole = this.add.ellipse(pos.x, pos.y, 200, 150, 0x222);
+    const hole = this.add.image(pos.x, pos.y-80, 'hole');
     hole.setDepth(5); //ensure holes are below moles and hammer
+    hole.setScale(4);
     gameState.holes.push(hole);
   });
 
@@ -69,6 +73,13 @@ function update () {
   //hammer on cursor - will be replaced by ultrasonic sensor positions
   gameState.hammer.x = this.input.activePointer.x;
   gameState.hammer.y = this.input.activePointer.y;
+}
+
+function setBackground(scene){
+  const background = scene.add.image(0, 0, 'background').setOrigin(0,0);
+  background.setDepth(0); //bg is behind everything
+  background.setDisplaySize(config.width, config.height);
+  return background;
 }
 
 function showGameOver(scene){
@@ -109,11 +120,9 @@ function createTimerBar(scene) {
 
 function createHammer(scene) {
   const hammer = scene.add.image(0, 0, 'hammer');
-  hammer.setScale(12); //scale hammer image to desired size
+  hammer.setScale(10); //scale hammer image to desired size
   hammer.setOrigin(0.5, 0.5); //set origin to center of the image
   hammer.setDepth(10); //ensure hammer is always on top of other game objects
-  hammer.setPipeline('TextureTintPipeline'); //enable tinting for the hammer image
-  hammer.texture.setFilter(Phaser.Textures.FilterMode.NEAREST); //set filter mode to nearest for pixelated effect
   return hammer;
 }
 
@@ -123,12 +132,10 @@ function createMoles(scene) {
     //create mole for each hole
     const mole = scene.add.image(hole.x, hole.y+80, 'mole');
     mole.setScale(4);
-    mole.setPipeline('TextureTintPipeline');
-    mole.texture.setFilter(Phaser.Textures.FilterMode.NEAREST);
-  mole.setVisible(false); //hidden until it has been selected via activateRandomMole
-  mole.setDepth(6); //ensure mole is above holes but below hammer
-  //mole starts lower down and will pop up when activated
-  gameState.moles.push(mole); //store reference to the mole
+    mole.setVisible(false); //hidden until it has been selected via activateRandomMole
+    mole.setDepth(6); //ensure mole is above holes but below hammer
+    //mole starts lower down and will pop up when activated
+    gameState.moles.push(mole); //store reference to the mole
   });
 }
 
@@ -141,6 +148,7 @@ function activateRandomMole(scene) {
   //random mole selection
   const randomMoleIndex = Phaser.Math.Between(0, gameState.moles.length - 1);
   const selectedMole = gameState.moles[randomMoleIndex];
+  selectedMole.setTexture('mole');
   //animate mole popping up
   const hole = gameState.holes[randomMoleIndex];
   gameState.activeMole = selectedMole;
@@ -155,7 +163,7 @@ function activateRandomMole(scene) {
   gameState.timerBar.bar.width = gameState.timerBar.maxWidth; //reset timer bar width
   gameState.timerBar.container.setVisible(true); //show timer bar
   gameState.timerBar.container.x = hole.x;
-  gameState.timerBar.container.y = hole.y - 170; //position timer bar above the mole
+  gameState.timerBar.container.y = hole.y - 60; //position timer bar above the mole
   //start time
   gameState.timerTween = scene.tweens.add({//animate mole popping up
     targets: gameState.timerBar.bar,
@@ -166,6 +174,7 @@ function activateRandomMole(scene) {
       gameState.timerTween = null; //reset timer tween reference
       //only decativeate mole if it is still the active mole (it might have been whacked)
       if (gameState.activeMole === selectedMole) {
+        selectedMole.setTexture('missed-mole');
         gameState.missedMoles++; 
         deactivateMole(scene, selectedMole);
        if(gameState.missedMoles >= gameState.maxMissedMoles){
@@ -177,7 +186,7 @@ function activateRandomMole(scene) {
 
   scene.tweens.add({//animate mole popping up
     targets: selectedMole,
-    y: hole.y + Mole_Active_Y, //move mole up
+    y: hole.y - 5, //move mole up
     duration: 300,
     ease: "Back.easeOut",
     onComplete: () => {
@@ -210,7 +219,7 @@ function deactivateMole(scene, mole) {
   const hole = gameState.holes[index];
   scene.tweens.add({
     targets: mole,
-    y: hole.y, //move mole down
+    y: hole.y+80, //move mole down
     duration: 300,
     ease: "Back.easeIn",
     onComplete: () => {
@@ -231,7 +240,7 @@ function whackMole(scene) {
 
   scene.tweens.add({
     targets: gameState.hammer,
-    angle: -35,
+    angle: -40,
     duration: 80,
     yoyo: true,
   });
@@ -243,6 +252,7 @@ function whackMole(scene) {
   const hammerY = gameState.hammer.y;
   const distance = Phaser.Math.Distance.Between(hammerX, hammerY, mole.x, mole.y);
   if (distance < 80) { //if hammer is close enough to the mole
+    mole.setTexture('whacked-mole');
     gameState.score += 1; //increase score
     gameState.scoreText.setText('Score: ' + gameState.score); //update score text
     deactivateMole(scene, mole); //deactivate the mole
@@ -254,6 +264,7 @@ const config = {
   width: 1200,
   height: 800,
   backgroundColor: "#93da60",
+  pixelArt: true,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
